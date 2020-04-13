105851e3a93891aed84cbe98104106d3c04253f5
Thimo Feller <thethimo98@gmail.com>
2020-04-13 17:00:21 +0200

    Collapsing columns

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -33,6 +33,15 @@ public class Board : MonoBehaviour
                 backgroundTile.name = "( " + i + ", " + j + " )";
 
                 int iconToUse = Random.Range(0, icons.Length);
+
+                int maxIterations = 0;
+                while(MatchesAt(i, j, icons[iconToUse]) && maxIterations < 100)
+                {
+                    iconToUse = Random.Range(0, icons.Length);
+                    maxIterations++;
+                }
+                maxIterations = 0;
+
                 GameObject icon = Instantiate(icons[iconToUse], tempPosition, Quaternion.identity);
                 icon.transform.parent = this.transform;
                 icon.name = "( " + i + ", " + j + " )";
@@ -40,4 +49,87 @@ public class Board : MonoBehaviour
             }
         }
     }
+
+    private bool MatchesAt(int column, int row, GameObject icon)
+    {
+        if(column > 1 && row > 1)
+        {
+            if(allIcons[column -1, row].tag == icon.tag && allIcons[column -2, row].tag == icon.tag)
+            {
+                return true;
+            }
+            if (allIcons[column, row - 1].tag == icon.tag && allIcons[column, row -2].tag == icon.tag)
+            {
+                return true;
+            }
+        } else if(column <= 1 || row <= 1)
+        {
+            if(row > 1)
+            {
+                if(allIcons[column, row -1].tag == icon.tag && allIcons[column, row - 2].tag == icon.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1)
+            {
+                if (allIcons[column -1, row].tag == icon.tag && allIcons[column -2, row].tag == icon.tag)
+                {
+                    return true;
+                }
+            }
+        }
+        
+
+        return false;
+    }
+
+    private void DestroyMatchesAt(int column, int row)
+    {
+        if(allIcons[column, row].GetComponent<Icon>().isMatched)
+        {
+            Destroy(allIcons[column, row]);
+            allIcons[column, row] = null;
+        }
+    }
+
+    public void DestroyMatches()
+    {
+        for(int i = 0; i< width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                if(allIcons[i, j] != null)
+                {
+                    DestroyMatchesAt(i, j);
+                }
+
+            }
+        }
+        StartCoroutine(DecreaseRowCo());
+    }
+
+    private IEnumerator DecreaseRowCo()
+    {
+        int nullcount = 0;
+        for(int i = 0; i<width; i++)
+        {
+            for(int j = 0; j<height; j++)
+            {
+                if(allIcons[i, j] == null)
+                {
+                    nullcount++;
+                }
+                else if(nullcount>0)
+                {
+                    allIcons[i, j].GetComponent<Icon>().row -= nullcount;
+                    allIcons[i, j] = null;
+                }
+            }
+            nullcount = 0;
+        }
+        yield return new WaitForSeconds(.4f);
+
+    }
+
 }
--- Assets/Scripts/Icon.cs
+++ Assets/Scripts/Icon.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Icon : MonoBehaviour
 {
 
+    [Header("Board Variables")]
     public int column;
     public int row;
+    public int previousColumn;
+    public int previousRow;
     public int targetX;
     public int targetY;
+    public bool isMatched = false;
+
     private Board board;
     private GameObject otherIcon;
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
     private Vector2 tempPosition;
     public float swipeAngle = 0;
+    public float swipeResist = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,25 +30,36 @@ public class Icon : MonoBehaviour
         targetY = (int)transform.position.y;
         row = targetY;
         column = targetX;
+        previousRow = row;
+        previousColumn = column;
     }
 
     // Update is called once per frame
     void Update()
     {
+        FindMatches();
+
+        if (isMatched)
+        {
+            SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
+            mySprite.color = new Color(1f, 1f, 1f, .2f);
+        }
         targetX = column;
         targetY = row;
         if(Mathf.Abs(targetX - transform.position.x) > .1)
         {
             //move towards target
             tempPosition = new Vector2(targetX, transform.position.y);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
-
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .6f);
+            if(board.allIcons[column, row] != this.gameObject)
+            {
+                board.allIcons[column, row] = this.gameObject;
+            }
         } else
         {
             //set position absolutely
             tempPosition = new Vector2(targetX, transform.position.y);
             transform.position = tempPosition;
-            board.allIcons[column, row] = this.gameObject;
         }
 
 
@@ -50,19 +67,42 @@ public class Icon : MonoBehaviour
         {
             //move towards target
             tempPosition = new Vector2(transform.position.x, targetY);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
-
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .6f);
+            if (board.allIcons[column, row] != this.gameObject)
+            {
+                board.allIcons[column, row] = this.gameObject;
+            }
         }
         else
         {
             //set position absolutely
             tempPosition = new Vector2(transform.position.x, targetY);
             transform.position = tempPosition;
-            board.allIcons[column, row] = this.gameObject;
         }
     }
 
-    void OnMouseDown()
+    public IEnumerator CheckMoveCo()
+    {
+        yield return new WaitForSeconds(.5f);
+        if(otherIcon != null)
+        {
+            if (!isMatched && !otherIcon.GetComponent<Icon>().isMatched)
+            {
+                otherIcon.GetComponent<Icon>().row = row;
+                otherIcon.GetComponent<Icon>().column = column;
+                row = previousRow;
+                column = previousColumn;
+
+            }
+            else
+            {
+                board.DestroyMatches();
+            }
+            otherIcon = null;
+        }
+    }
+
+    private void OnMouseDown()
     {
         firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -76,20 +116,22 @@ public class Icon : MonoBehaviour
 
     void CalculateAngle()
     {
-        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
-        //Debug.Log(swipeAngle);
-        MoveIcons();
+        if(Mathf.Abs(finalTouchPosition.y - firstTouchPosition.y) > swipeResist || Mathf.Abs(finalTouchPosition.x - firstTouchPosition.x) > swipeResist)
+        {
+            swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+            MoveIcons();
+        }
     }
 
     void MoveIcons()
     {
-        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width)
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width - 1)
         {
             //Right
             otherIcon = board.allIcons[column + 1, row];
             otherIcon.GetComponent<Icon>().column -= 1;
             column += 1;
-        } else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)
+        } else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height - 1)
         {
             //Up
             otherIcon = board.allIcons[column, row + 1];
@@ -107,6 +149,41 @@ public class Icon : MonoBehaviour
             otherIcon = board.allIcons[column, row - 1];
             otherIcon.GetComponent<Icon>().row += 1;
             row -= 1;
+        }
+        StartCoroutine(CheckMoveCo());
+    }
+
+    void FindMatches()
+    {
+        if(column > 0 && column < board.width - 1)
+        {
+            GameObject leftIcon1 = board.allIcons[column - 1, row];
+            GameObject rightIcon1 = board.allIcons[column + 1, row];
+            if(leftIcon1 != null && rightIcon1 != null)
+            {
+                if (leftIcon1.tag == this.gameObject.tag && rightIcon1.tag == this.gameObject.tag)
+                {
+                    leftIcon1.GetComponent<Icon>().isMatched = true;
+                    rightIcon1.GetComponent<Icon>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+           
+        }
+        if (row > 0 && row < board.height - 1)
+        {
+            GameObject downIcon1 = board.allIcons[column, row - 1];
+            GameObject upIcon1 = board.allIcons[column, row + 1];
+            if (downIcon1 != null && upIcon1 != null)
+            {
+                if (downIcon1.tag == this.gameObject.tag && upIcon1.tag == this.gameObject.tag)
+                {
+                    downIcon1.GetComponent<Icon>().isMatched = true;
+                    upIcon1.GetComponent<Icon>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+            
         }
     }
 }
