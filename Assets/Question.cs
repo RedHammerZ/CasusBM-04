bde18e8ef9f22d07f636f7eb2b2a55f20f918628
Thijs <thijsvleugels@gmail.com>
2020-04-16 09:42:15 +0200

    Vraag gefixed

---
+++ Assets/Question.cs
@@ -0,0 +1,10 @@
+﻿
+[System.Serializable]
+public class Question
+{
+    public string vraag;
+    public string correctAntwoord;
+    public string foutAntwoord1;
+    public string foutAntwoord2;
+    public string foutAntwoord3;
+}
